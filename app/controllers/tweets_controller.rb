8efe524ff8f4cec1497b44d3bc492d0473c19e64
Stephen Wright <scw248@gmail.com>
2019-03-07 14:07:17 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,33 @@
 class TweetsController < ApplicationController
+  get '/tweets/new' do 
+    erb :'tweets/new'
+  end
 
+  post '/tweets' do
+    @tweet = Tweet.new(params[:tweet])
+    redirect to "tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'tweets/edit'
+  end 
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(params["tweet"])
+    
+    redirect to "tweets/#{@tweet.id}"
+  end
+
+  delete 'tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    redirect to '/tweets'
+  end
 
 end
